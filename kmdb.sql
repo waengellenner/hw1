2f975922f88aa6d987f98ec3a85f262e527a30f2
Will Engellenner <will.engellenner@kellogg.northwestern.edu>
2023-04-19 15:22:59 +0000

    Updates cont'd

--- kmdb.sql
+++ kmdb.sql
@@ -142,7 +142,30 @@ CREATE TABLE roles (
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+INSERT INTO movies (
+  title,
+  year_released,
+  MPAA_rating,
+  studio_id
+)
+VALUES (
+  "Batman Begins",
+  2005,
+  "PG-13",
+  1
+),
+(
+  "The Dark Knight",
+  2008,
+  "PG-13",
+  1
+),
+(
+  "The Dark Knight Rises",
+  2012,
+  "PG-13",
+  1
+);
 
 -- Prints a header for the movies output
 .print "Movies"
